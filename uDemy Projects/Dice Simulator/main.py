10852be46887fc57ee653e8033aebadf95d733c8
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-01-02 21:42:11 -0500

    Update: Dice Simulator

--- uDemy Projects/Dice Simulator/main.py
+++ uDemy Projects/Dice Simulator/main.py
@@ -8,7 +8,26 @@ def roll_dice(amount: int = 2) -> list[int]:
   
   rolls: list[int] = []
   for i in range(amount):
-    random_roll: int = random.ranint(1, 6)
+    random_roll: int = random.randint(1, 6)
     rolls.append(random_roll)
 
   return rolls
+
+def main():
+  while True:
+    try: 
+      user_input: str = input("How many dice would you like to roll? ")
+      
+      if user_input.lower() == "exit":
+        print("Thanks for playing!")
+        break
+
+      print(*roll_dice(int(user_input)), sep=', ')
+
+    except ValueError:
+      print("(Please enter a valid number)")
+
+
+if __name__ == '__main__':
+  print("Type exit to quit the game")
+  main()
