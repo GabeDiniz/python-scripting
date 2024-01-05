6b9c12da2f137e070455cd662d902b4a2bfd5b28
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-01-05 16:50:24 -0500

    Update: RPS game

--- uDemy Projects/4 RockPaperScissors/main.py
+++ uDemy Projects/4 RockPaperScissors/main.py
@@ -5,35 +5,45 @@ class RPS:
   def __init__(self):
     print("Welcome to RPS!")
     self.moves: dict = {"rock": "💎", "paper": "🧻", "scissors": "✂"}
-    # Create list of keys from dictionary
+    # Create list of keys from dictionary 
     self.valid_moves: list[str] = list(self.moves.keys())
 
   def play_game(self):
-    user_move: str = input("Rock, paper, or scissors? >>").lower()
+    user_move = input("Rock, paper, or scissors? >> ").lower()
 
     if user_move == "exit":
       print("Thanks for playing!")
       sys.exit()
 
-      if user_move not in self.valid_moves:
-        print("Invalid moves...")
-        return self.play_game
-      
-      # Give AI raandom move
-      ai_move: str = random.choice(self.valid_moves)
+    if user_move not in self.valid_moves:
+      print("Invalid moves...")
+      return self.play_game
+    
+    # Give AI raandom move
+    ai_move: str = random.choice(self.valid_moves)
 
-      self.display_moves(user_move, ai_move)
-      self.check_move(user_move, ai_move)
+    self.display_moves(user_move, ai_move)
+    self.check_move(user_move, ai_move)
   
-  def display_moves(self):
-    ...
+  def display_moves(self, user_move: str, ai_move: str):
+    print("-----")
+    print(f"You: {self.moves[user_move]}")
+    print(f"AI: {self.moves[ai_move]}")
+    print("-----")
 
-  def check_move(self):
-    ...
-
-def RPS():
-  print
-
+  def check_move(self, user_move: str, ai_move: str):
+    if user_move == ai_move: # TIE
+      print("It\'s a tie!")
+    elif user_move == "rock" and ai_move == "scissors":
+      print("You win!")
+    elif user_move == "scissors" and ai_move == "paper":
+      print("You win!")
+    elif user_move == "paper" and ai_move == "rock":
+      print("You win!")
+    else:
+      print("AI wins...")
 
 if __name__ == "__main__":
   rps = RPS()
+  while True:
+    rps.play_game()
