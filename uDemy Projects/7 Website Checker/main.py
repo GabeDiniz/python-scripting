d1b7058e7caa95b7d1b6a2375449333670f5efcb
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-01-07 20:27:16 -0500

    Update: get status and check website functionality

--- uDemy Projects/7 Website Checker/main.py
+++ uDemy Projects/7 Website Checker/main.py
@@ -19,3 +19,33 @@ def get_websites(csv_path: str) -> list[str]:
       
     return websites
   
+def get_user_agent() -> str:
+  ua = UserAgent()
+  return ua.chrome
+
+def get_status_description(status_code: int) -> str:
+  # Check for different HTTP Status (to see the list type: HTTPStatus.)
+  for value in HTTPStatus:
+    if value == status_code:
+      description: str = f"({value} {value.name}) {value.description}"
+      return description
+  
+  return "(???) Unknown status code..."
+
+def check_website(website: str, user_agent):
+  try:
+    code: int = requests.get(website, headers={'User-Agent': user_agent}).status_code
+    print(website, get_status_description(code))
+  except Exception:
+    print(f"**Could not get information for website: '{website}'")
+
+def main():
+  sites: list[str] = get_websites("websites.csv")
+  user_agent: str = get_user_agent()
+
+  # Iterate through list of sites
+  for site in sites:
+    check_website(site, user_agent)
+
+if __name__ == "__main__":
+  main()
