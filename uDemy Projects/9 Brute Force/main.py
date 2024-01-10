d991b8850b7b1b25d77a88372953646b8bd1fd1e
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-01-09 21:58:11 -0500

    Update: main functions

--- uDemy Projects/9 Brute Force/main.py
+++ uDemy Projects/9 Brute Force/main.py
@@ -2,3 +2,36 @@ import itertools
 import string
 import time
 
+def common_guess(word: str) -> str | None:
+  with open("words.txt", "r") as words:
+    word_list: list[str] = words.read().splitlines()
+
+  # Iterate through common words 
+  for i, match in enumerate(word_list, start = 1):
+    if match == word:
+      return f"Common match: {match} (#{i})"
+    
+def brute_force(word: str, length: int, digits: bool = False, symbols: bool = False) -> str | None:
+  chars: str = string.ascii_lowercase
+
+  if digits:
+    chars += string.digits
+  if symbols:
+    chars += string.punctuation
+  
+  attempts: int = 0
+  for guess in itertools.product(chars, repeat=length): # Itertools will brute force possible combinations
+    attempts += 1
+    guess: str = ''.join(guess)
+
+    if guess == word:
+      return f'"{word}" was cracked in {attempts:,} guesses.'
+    
+    # print(guess, attempts)
+
+def main():
+  print("Searching...")
+  password: str = "abc1"
+
+if __name__ == "__main__":
+  print(common_guess("car"))
