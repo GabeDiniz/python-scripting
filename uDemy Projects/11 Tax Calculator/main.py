9ef4cda782bfb6a6b7a812b717ca350c1d426742
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-01-11 19:39:41 -0500

    Update: Tax Calculator

--- uDemy Projects/11 Tax Calculator/main.py
+++ uDemy Projects/11 Tax Calculator/main.py
@@ -1,1 +1,58 @@
 import customtkinter as ctk # pip install customtkinter
+
+# IDEAS FOR IMPROVEMENT
+# Add a button to calculate off of Ontario income tax based on the tax bracket the income is in. 
+
+class TaxCalculator:
+  def __init__(self) -> None:
+    # Initialize the window
+    self.window = ctk.CTk()
+    self.window.title("Tax Calculator")
+    self.window.geometry("280x200")
+    self.window.resizable(False, False)
+
+    self.padding: dict = {"padx": 20, "pady": 10}
+
+    # Income label and entry
+    self.income_label = ctk.CTkLabel(self.window, text = "Income: ")
+    self.income_label.grid(row = 0, column = 0, **self.padding)
+    self.income_entry = ctk.CTkEntry(self.window)
+    self.income_entry.grid(row = 0, column = 1, **self.padding)
+
+    # Tax label and entry
+    self.tax_rate_label = ctk.CTkLabel(self.window, text = "Percent: ")
+    self.tax_rate_label.grid(row = 1, column = 0, **self.padding)
+    self.tax_rate_entry = ctk.CTkEntry(self.window)
+    self.tax_rate_entry.grid(row = 1, column = 1, **self.padding)
+
+    # Result label and entry
+    self.result_label = ctk.CTkLabel(self.window, text = "Tax: ")
+    self.result_label.grid(row = 2, column = 0, **self.padding)
+    self.result_entry = ctk.CTkEntry(self.window)
+    self.result_entry.insert(0, "0")
+    self.result_entry.grid(row = 2, column = 1, **self.padding)
+
+    # Calculate button
+    self.calculate_button = ctk.CTkButton(self.window, text = "Calculate", command = self.calculate_tax)
+    self.calculate_button.grid(row = 3, column = 1, **self.padding)
+
+  def update_result(self, text: str):
+    self.result_entry.delete(0, ctk.END)
+    self.result_entry.insert(0, text)
+
+  def calculate_tax(self):
+    try: 
+      income: float = float(self.income_entry.get())
+      tax_rate: float = float(self.tax_rate_entry.get())
+      self.update_result(f"${income * (tax_rate / 100):,.2f}") 
+
+    except ValueError:
+      self.update_result("Invalid input")
+
+
+  def run(self):
+    self.window.mainloop()
+
+if __name__ == "__main__":
+  tc = TaxCalculator()
+  tc.run()
