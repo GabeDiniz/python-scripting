2052c9176fe3cc18cd6f5bf1b6bf6d16cf77d7da
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-01-16 19:45:18 -0500

    Init: URL Shortner

---
+++ uDemy Projects/14 URL Shortner/main.py
@@ -0,0 +1,9 @@
+'''
+Prequisite: An API key needs to be generated from:
+  >> https://cutt.ly/edit
+'''
+
+from typing import Final
+import requests
+
+API_KEY: Final[str] = "ccf711b722e1d3d40880158e8dbfee1d882f7"
