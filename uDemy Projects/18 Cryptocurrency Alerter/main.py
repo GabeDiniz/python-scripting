b9bfc332d5d65950346e1ce9c976acbc58feaf9e
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-01-21 11:39:54 -0500

    Update: crypto-currency main

--- uDemy Projects/18 Cryptocurrency Alerter/main.py
+++ uDemy Projects/18 Cryptocurrency Alerter/main.py
@@ -3,14 +3,16 @@ from crypto_data import get_coins, Coin
 def alert(symbol: str, bottom: float, top: float, coins_list: list[Coin]):
   for coin in coins_list:
     if coin.symbol == symbol:
-      if coin.current_price > top or coin.current_price < bottom:
-        print(coin, "!!!TRIGGERED!!!")
+      if coin.current_price > top:
+        print(coin, "!!! Price is HIGH !!!")
+      elif coin.current_price < bottom:
+        print(coin, "!!! Price is LOW !!!")
       else: 
         print(coin)
 
 if __name__ == "__main__":
   coins: list[Coin] = get_coins()
 
-  alert("btc", bottom=20_000, top=24_000, coins_list=coins)
-  alert("eth", bottom=1800, top=1900, coins_list=coins)
-  alert("xrp", bottom=0.47, top=0.48, coins_list=coins)
+  alert("btc", bottom=30000, top=49955.78, coins_list=coins)
+  alert("eth", bottom=3000, top=3405.34, coins_list=coins)
+
