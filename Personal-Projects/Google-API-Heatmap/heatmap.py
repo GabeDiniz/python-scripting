d8f8cb4882b92f57db0feb874a1a21f796ac0500
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-01-24 19:31:43 -0500

    Update: clean up heatmap code

--- Personal-Projects/Google-API-Heatmap/heatmap.py
+++ Personal-Projects/Google-API-Heatmap/heatmap.py
@@ -4,20 +4,15 @@ from folium.plugins import HeatMap
 import os
 
 print("Current Working Directory:", os.getcwd())
-os.chdir('C:/Users/gabri/Desktop/Code/Misc/python-scripting/Personal Projects/Google API Heatmap')
 
 # Load geocoded addresses
 data = pd.read_csv('geocoded_addresses.csv')
 
 # Prepare the data for the heatmap
-# Ensure that your latitude and longitude are correctly indexed
-print(data.iterrows())
-# heat_data = [print(row["Latitude"]) for index, row in data.iterrows()]
 heat_data = []
 for _, row in data.iterrows():
-  print(row)
   if pd.notnull(row['Latitude']) and pd.notnull(row['Longitude']):
-        heat_data.append([row['Latitude'], row['Longitude']])
+    heat_data.append([row['Latitude'], row['Longitude']])
 
 # Create a map centered around an average location
 map_center = [data['Latitude'].mean(), data['Longitude'].mean()]
@@ -27,4 +22,5 @@ map = folium.Map(location=map_center, zoom_start=12)
 HeatMap(heat_data).add_to(map)
 
 # Save it to an HTML file
-map.save('C:/Users/gabri/Desktop/Code/Misc/python-scripting/Personal Projects/Google API Heatmap/heatmap.html')
+print("Heatmap generated successfully...")
+map.save('heatmap.html')
