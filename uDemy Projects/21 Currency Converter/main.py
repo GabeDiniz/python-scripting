8645afc4af3ca903fd391e395c24e0ad4ab6557c
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-01-30 22:32:37 -0500

    Update: currency-converter

--- uDemy Projects/21 Currency Converter/main.py
+++ uDemy Projects/21 Currency Converter/main.py
@@ -20,9 +20,34 @@ def get_rates(mock: bool = False) -> dict:
   request = requests.get(url=BASE_URL, params=payload)
   data: dict = request.json()
 
-  with open("rates.json", "w") as file:
-    return json.dump(data, file)
+  # with open("rates.json", "w") as file:
+  #   return json.dump(data, file)
   
   return data
 
-get_rates()
+def get_currency(currency: str, rates: dict) -> float:
+  currency: str = currency.upper()
+  if currency in rates.keys():
+    return rates.get(currency)
+  else:
+    raise ValueError(f'"{currency}" is not a valid currency')
+
+def convert_currency(amount: float, base: str, vs: str, rates: dict) -> float:
+  base_rate: float = get_currency(base, rates)
+  vs_rate: float = get_currency(vs, rates)
+
+  conversion: float = round((vs_rate / base_rate) * amount, 2)
+  print(f'{amount:,.2f} ({base}) is: {conversion:,.2f} ({vs})') # if number is greater than 10000 it will place a decimal -> 10,000
+  return conversion
+
+
+# print(get_rates(mock=True))
+
+def main():
+  data: dict = get_rates(mock=True)
+  rates: dict = data.get("rates")
+
+  convert_currency(1, "USD", "CAD", rates=rates)
+
+if __name__ == "__main__":
+  main()
