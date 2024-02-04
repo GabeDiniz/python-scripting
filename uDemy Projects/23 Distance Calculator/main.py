580ebf6099f52821cc7b1fa9a2a632bb1f41bc4c
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-02-04 12:27:53 -0500

    Update: distance-calculator

--- uDemy Projects/23 Distance Calculator/main.py
+++ uDemy Projects/23 Distance Calculator/main.py
@@ -20,11 +20,30 @@ def get_coordinates(address: str) -> Coordinates | None:
     return Coordinates(latitude = location.latitude, longitude = location.longitude)
   
 
-def main():
-  home_address: str = input("Enter Home Address >> ")
-  destination_address: str = input("Enter Destination Address >> ")
+def calculate_distance_km(home: Coordinates, target: Coordinates) -> float | None:
+  if home and target:
+    distance: float = geodesic(home.coordinates(), target.coordinates()).kilometers
+    return distance
+  
 
-  print(get_coordinates(home_address))
+def get_distance_km(home: str, target: str) -> float | None:
+  home_coordinates: Coordinates = get_coordinates(home)
+  target_coordinates: Coordinates = get_coordinates(target)
+
+  if distance := calculate_distance_km(home_coordinates, target_coordinates):
+    print(f"{home} -> {target}")
+    print(f"{distance:.2f} kilometers")
+    return distance
+  else:
+    print("Failed to calculate the distance.")
+
+
+def main():
+  home_address: str = input("Enter From Destination >> ")
+  destination_address: str = input("Enter to Destination >> ")
+
+  print("Calculating...")
+  get_distance_km(home_address, target=destination_address)
 
 if __name__ == "__main__":
   main()
