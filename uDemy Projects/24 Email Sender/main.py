b53e111d100caa4b06b945716c448c1ee5ab5319
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-02-05 18:27:47 -0500

    Update: send mail functionality

--- uDemy Projects/24 Email Sender/main.py
+++ uDemy Projects/24 Email Sender/main.py
@@ -11,8 +11,45 @@ from decouple import config
 EMAIL = config('EMAIL')
 PASSWORD = config('EMAIL_PASS')
 
+
+def create_image_attachment(path: str) -> MIMEImage:
+  raise NotImplementedError()
+
+def send_email(to_email: str, subject: str, body: str, image: str | None = None):
+  # Host and Port for sending emails
+  host: str = "smtp-mail.outlook.com"
+  port: int = 587
+
+  context = ssl.create_default_context()
+
+  with smtplib.SMTP(host, port) as server:
+    print("Logging in...")
+    server.ehlo()
+    server.starttls(context=context)
+    server.ehlo()
+    server.login(EMAIL, PASSWORD)
+
+    # Prepare the email
+    print("Attempting to send email")
+    message = MIMEMultipart()
+    message["From"] = EMAIL
+    message["To"] = to_email
+    message["Subject"] = subject
+    message.attach(MIMEText(body, "plain"))
+
+    if image:
+      file: MIMEImage = create_image_attachment()
+      message.attach(file)
+
+    server.sendmail(from_addr = EMAIL, to_addrs = to_email, msg = message.as_string())
+
+    # Success!
+    print("Email sent!")
+
+
 def main():
   print(EMAIL, PASSWORD)
+
 
 if __name__ == "__main__":
   main()
