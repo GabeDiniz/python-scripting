7cf15f439039b56ba4f9b28553c654b1e247b74d
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-02-08 19:11:31 -0500

    Update: add dummy_data caching

--- uDemy Projects/25 Weather App/weather_api.py
+++ uDemy Projects/25 Weather App/weather_api.py
@@ -1,3 +1,4 @@
+import json
 import requests   # pip install requests
 from typing import Final
 
@@ -10,4 +11,20 @@ BASE_URL: Final[str] = "https://api.openweathermap.org/data/2.5/forecast"
 
 
 def get_weather(city_name: str, mock: bool = True) -> dict:
-  pass
+  if mock:
+    with open("dummy_data.json") as file:
+      return json.load(file)
+    
+  # Request live data
+  payload: dict = {"q": city_name, "appid": API_KEY, "units": "metric"}
+  request = requests.get(url=BASE_URL, params=payload)
+  data: dict = request.json()
+
+  # Puts API data into JSON file
+  with open("dummy_data.json", "w") as file:
+    json.dump(data, file)
+
+  return data
+
+if __name__ == "__main__":
+  print(get_weather("Toronto", mock=True))
