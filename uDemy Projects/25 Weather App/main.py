7b701404c665a8fd45ce0608a4af99472b6d727c
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-02-08 21:15:00 -0500

    Update: finalize Weather app

--- uDemy Projects/25 Weather App/main.py
+++ uDemy Projects/25 Weather App/main.py
@@ -1,5 +1,6 @@
 import requests   # pip install requests
 from typing import Final
+from weather_api import get_weather, get_weather_details, Weather
 
 # Fetch Credentials from local .env variables 
 from decouple import config
@@ -7,3 +8,27 @@ from decouple import config
 # Email Credentials
 API_KEY = config('WEATHER_API_KEY')
 BASE_URL: Final[str] = "https://api.openweathermap.org/data/2.5/forecast"
+
+def main():
+  user_city: str = input("Enter a city: ")
+
+  # Get the current weather details
+  current_weather: dict = get_weather(user_city, mock=False)
+  weather_details: list[Weather] = get_weather_details(current_weather)
+
+  # Get the current days
+  dfmt: str = "%d/%m/%y"
+  days: list[str] = sorted(list({f"{date.date:{dfmt}}" for date in weather_details}))
+  
+  for day in days:
+    print(day)
+    print("-"*10)
+
+    grouped: list[Weather] = [current for current in weather_details if f"{current.date:{dfmt}}" == day]
+    for element in grouped: 
+      print(element)
+
+    print("")
+
+if __name__ == "__main__":
+  main()
--- uDemy Projects/25 Weather App/weather_api.py
+++ uDemy Projects/25 Weather App/weather_api.py
@@ -1,6 +1,7 @@
 import json
 import requests   # pip install requests
 from typing import Final
+from model import Weather, dt
 
 # Fetch Credentials from local .env variables 
 from decouple import config
@@ -26,5 +27,29 @@ def get_weather(city_name: str, mock: bool = True) -> dict:
 
   return data
 
+def get_weather_details(weather: dict) -> list[Weather]:
+  days: list[dict] = weather.get("list")
+
+  # If no data -> raise exception
+  if not days:
+    raise Exception(f"[Error] Problem with json: {weather}")
+  
+  list_of_weather: list[Weather] = []
+  for day in days:
+    w: Weather = Weather(date = dt.fromtimestamp(day.get('dt')),
+                         details = (details := day.get("main")),
+                         temp = details.get("temp"),
+                         weather = (weather := day.get("weather")),
+                         description = weather[0].get("description"))
+    list_of_weather.append(w)
+
+  return list_of_weather
+
+
 if __name__ == "__main__":
-  print(get_weather("Toronto", mock=True))
+  current_weather: dict = get_weather("Toronto", mock=True)
+  weather: list[Weather] = get_weather_details(current_weather)
+
+  for w in weather:
+    print(w)
+    print(w.temp, w.details)
