00f535edf5afed8645c5e0220d3a12a5c06a5ab1
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-02-19 14:03:48 -0500

    Add: responses functionality

--- uDemy Projects/28 Discord Bot/responses.py
+++ uDemy Projects/28 Discord Bot/responses.py
@@ -1,2 +1,30 @@
 from difflib import get_close_matches
 import json
+
+# Returns the best matched question based on knowledge
+def get_best_match(user_question: str, questions: dict) -> str | None:
+  questions: list[str] = [q for q in questions]
+  matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6) # Only 1 match possible
+
+  # Return best match
+  if matches:
+    return matches[0]
+  
+def get_response(message: str, knowledge: dict) -> str:
+  best_match: str | None = get_best_match(message, knowledge)
+
+  if answer := knowledge.get(best_match): 
+    return answer
+  else: # Default message
+    return "I don\'t understand..."
+  
+def load_knowledge(file: str) -> dict:
+  with open(file, "r") as f:
+    return json.load(f)
+  
+
+
+if __name__ == "__main__":
+  test_knowledge: dict = load_knowledge("knowledge.json")
+  test_response: str = get_response("who is you?", knowledge=test_knowledge)
+  print(test_response)
