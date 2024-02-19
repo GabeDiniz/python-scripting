05068f2ca2f6f3b8963ae6192e493bd114088cff
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-02-18 19:19:37 -0500

    Setup: telegram bot constants

--- uDemy Projects/27 Telegram Bot/main.py
+++ uDemy Projects/27 Telegram Bot/main.py
@@ -1,3 +1,14 @@
 # Telegram Bot
 # Name: Mario
 # username: GabeDini_bot
+from typing import Final
+# pip install python-telegram-bot
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+# Fetch Credentials from local .env variables 
+from decouple import config
+
+# Constants
+TOKEN = config('BOT_API_KEY')
+BOT_USERNAME: Final[str] = "@GabeDini_bot"
