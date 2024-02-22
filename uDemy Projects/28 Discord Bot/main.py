d45c1fae1693f1e7138471a19dbf5a537e180744
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-02-21 21:36:48 -0500

    Fix: custom !help command embed

--- uDemy Projects/28 Discord Bot/main.py
+++ uDemy Projects/28 Discord Bot/main.py
@@ -1,4 +1,5 @@
-from discord import Intents, Client   # pip install discord
+from discord import Intents, Client
+import discord   # pip install discord
 import responses
 import help
 
@@ -36,23 +37,28 @@ def run_bot(BOT_KEY: str):
     # Check for specific user
     # if str(message.author) == "username here":
     #   response: str = "Oh hello there... I've been expecting you"
+    response = None
     if message.content:
       print(f'({message.channel}) {message.author}: "{message.content}"')
+      
       # Handle !help command
       if message.content == "!help":
-        response: str = help.format()   # may not be a string
-
-        # response: str = ''.join([
-        #   "Here are my current commands!",
-        #   "\n```",  # START Markdown CODE BLOCK
-        #   "!help - List commands available",
-        #   "\n!command - Command description",
-        #   "```"   # END Markdown CODE BLOCK
-        # ])
+        embed = discord.Embed(
+            title='Help',
+            description='This is a help message!',
+            color=discord.Color.blue()
+        )
+        # Add fields to the embed (optional)
+        embed.add_field(name='Command 1', value='Description 1', inline=False)
+        embed.add_field(name='Command 2', value='Description 2', inline=False)
+        
+        # Send the embed message to the same channel where the command was issued
+        await message.channel.send(embed=embed)
       else:
         response: str = responses.get_response(message.content, knowledge=knowledge)
-    
       if response:
+        # Sending message
+        print(response)
         await message.channel.send(response)
       
     # Potential error: i.e., missing permissions to access message.content
