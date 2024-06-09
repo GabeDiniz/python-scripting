b65170010889c6e797b272def46e44f3cb867e4f
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-06-09 19:09:00 -0400

    Final: working game

--- uDemy Projects/30 Dodgy Square Game/main.py
+++ uDemy Projects/30 Dodgy Square Game/main.py
@@ -28,7 +28,7 @@ class DodgySquare:
 
     # Player
     self.player_size: int = 30 # square size
-    self.player_size: list[int] = [0, 0] # Player starting coordinates
+    self.player_pos: list[int] = [0, 0] # Player starting coordinates
 
     # Enemies
     self.enemy_size: int = 50 # enemy square size
@@ -69,12 +69,12 @@ class DodgySquare:
     px, py = player_pos
     ex, ey = enemy_pos
 
-    if (px <= ex < (px + self.player_size)) or (ex <= px < (ex * self.enemy_size)):
-      if (py <= ey < (py + self.player_size)) or (ey <= py < (ey * self.enemy_size)):
+    if (px <= ex < (px + self.player_size)) or (ex <= px < (ex + self.enemy_size)):
+      if (py <= ey < (py + self.player_size)) or (ey <= py < (ey + self.enemy_size)):
         return True
     return False
   
-  def game_over(self):
+  def show_game_over(self):
     game_over_text = self.font.render("Game Over", True, self.RED)
     self.screen.blit(game_over_text, (self.screen_width // 2 - 70, self.screen_height // 2 - 16))
 
@@ -90,4 +90,64 @@ class DodgySquare:
     self.score: int = 0
 
   def draw_character(self, color: tuple, position: list[int], size: int):
-    pygame.draw.rect(self.screen, color, (position[0], position[1]), size, size)
+    pygame.draw.rect(self.screen, color, (position[0], position[1], size, size))
+
+  def run(self):
+    while True:
+      for event in  pygame.event.get():
+        if event.type == pygame.QUIT:
+          pygame.quit()
+          sys.exit()
+
+        # Replay the game
+        if event.type == pygame.KEYDOWN:
+          if self.game_over and event.key == pygame.K_r:
+            self.replay_game()
+
+        # Get the mouse position
+        mouse_pos: tuple[int, int] = pygame.mouse.get_pos()
+
+        # Update the player's position to follow the mouse
+        self.player_pos[0] = mouse_pos[0] - self.player_size // 2
+        self.player_pos[1] = mouse_pos[1] - self.player_size // 2
+
+        # Make sure the player can't leave the screen
+        self.player_pos[0] = max(0, min(self.player_pos[0], self.screen_width - self.player_size))
+        self.player_pos[1] = max(0, min(self.player_pos[1], self.screen_height - self.player_size))
+        
+      if not self.game_over:
+        self.update_enemy_positions()
+        for enemy_pos in self.enemy_list:
+          if self.detect_collision(self.player_pos, enemy_pos):
+            self.game_over = True
+            break
+        
+        # Reset everything for the next frame
+        self.screen.fill(self.BLACK)
+
+        # Draw player
+        self.draw_character(self.WHITE, self.player_pos, self.player_size)
+
+        # Draw enemies
+        for enemy_pos in self.enemy_list:
+          if self.score > 100:
+            self.draw_character(self.BLUE, enemy_pos, self.enemy_size)
+          else:
+            self.draw_character(self.RED, enemy_pos, self.enemy_size)
+          
+        # Display the score
+        score_text = self.font.render(f'Score: {self.score}', True, self.WHITE)
+        self.screen.blit(score_text, [10, 10])
+
+        # Increment frame count
+        self.frame_count += 1
+      else:
+        self.show_game_over()
+      
+      pygame.display.update()
+      self.clock.tick(60)
+
+
+if __name__ == '__main__':
+  game = DodgySquare()
+  game.run()
