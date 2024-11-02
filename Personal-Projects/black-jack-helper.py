8805072cf26ec3b51f5909b15a72fd206f4ba0be
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-11-02 17:24:04 -0400

    [init] black-jack-helper script

---
+++ Personal-Projects/black-jack-helper.py
@@ -0,0 +1,11 @@
+#####################################################
+######################  USAGE  ######################
+#####################################################
+'''
+python black-jack-helper.py
+
+Description: 
+The script will ask you to enter the the dealers hand,
+  along with your hand and it will tell you what the
+  blackjack book recommends you to do. 
+'''
