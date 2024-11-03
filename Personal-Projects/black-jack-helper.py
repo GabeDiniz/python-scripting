6545d2107ba94e3fe6af52af1eeaa63d83fea9ad
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2024-11-03 11:15:10 -0500

    [wip] finding optimal next move

--- Personal-Projects/black-jack-helper.py
+++ Personal-Projects/black-jack-helper.py
@@ -9,3 +9,28 @@ The script will ask you to enter the the dealers hand,
   along with your hand and it will tell you what the
   blackjack book recommends you to do. 
 '''
+
+
+def get_player_cards():
+  print("Card options: [A, 2-10, J, Q, K]")
+  card1 = input("Enter your first card: ").lower()
+  card2 = input("Enter your second card: ").lower()
+
+  return [card1, card2]
+
+def next_move(players_hand, dealers_hand):
+  # base
+  if ('8' in players_hand and '3' in players_hand) or ('8' in players_hand and '4' in players_hand):
+    print("Book says HIT")
+  elif ('A' in players_hand and '8' in players_hand) or ('A' in players_hand and '9' in players_hand):
+    print("Book says HIT")
+
+if __name__ == "__main__":
+  players_hand =  get_player_cards()
+  dealers_hand = [input("Enter dealers card: ").lower()]
+  print(players_hand)
+  print(dealers_hand)
+
+  while True: 
+    next_move(players_hand, dealers_hand)
+    break
